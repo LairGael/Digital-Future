f4311d5f99d6a8bbb9947cbf6196ab04f4cb7a8f
LairGael <lairurbieta@gmail.com>
2022-07-28 15:52:52 -0500

    Cambios mayores

--- js/servicios.js
+++ js/servicios.js
@@ -1,6 +1,16 @@
 const servicio1 = document.getElementById('servicio1');
 const servicio2 = document.getElementById('servicio2');
 const servicio3 = document.getElementById('servicio3');
+const servicio4 = document.getElementById('servicio4');
+const servicio5 = document.getElementById('servicio5');
+const servicioG1 = document.getElementById('servicioG1');
+const servicioG2 = document.getElementById('servicioG2');
+const servicioG3 = document.getElementById('servicioG3');
+const servicioG4 = document.getElementById('servicioG4');
+const servicioG5 = document.getElementById('servicioG5');
+const servicioG6 = document.getElementById('servicioG6');
+const servicioA1 = document.getElementById('servicioA1');
+const servicioA2 = document.getElementById('servicioA2');
 
 
 const cargarElementoServicio = (entradas, observadorServicio) => {
@@ -24,3 +34,13 @@ const observadorServicio = new IntersectionObserver(cargarElementoServicio, {
 observadorServicio.observe(servicio1);
 observadorServicio.observe(servicio2);
 observadorServicio.observe(servicio3);
+observadorServicio.observe(servicio4);
+observadorServicio.observe(servicio5);
+observadorServicio.observe(servicioG1);
+observadorServicio.observe(servicioG2);
+observadorServicio.observe(servicioG3);
+observadorServicio.observe(servicioG4);
+observadorServicio.observe(servicioG5);
+observadorServicio.observe(servicioG6);
+observadorServicio.observe(servicioA1);
+observadorServicio.observe(servicioA2);
